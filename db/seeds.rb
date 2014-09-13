47cd9f7a336a398243248502c910f5c89e712fe9
Peng Wang <randywang001@gmail.com>
2014-09-12 22:59:32 -0700

    Make listing pretty, add cutomized css, link stylesheet to pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.delete_all
+
+Product.create(:title => 'Programming Ruby 1.0',
+	:description =>
+	%{<p>
+        Ruby is the fastest growing and most exciting dynamic language out
+        there. If you need to get working programs delivered fast, you should
+        add Ruby to your toolbox.
+	</p>},
+	:image_url => 'ror1.jpg',
+	:price => 49.52)
+Product.create(:title => 'Programming Ruby 1.1',
+	:description =>
+	%{<p>
+        Ruby is the fastest growing and most exciting dynamic language out
+        there. If you need to get working programs delivered fast, you should
+        add Ruby to your toolbox.
+	</p>},
+	:image_url => 'ror2.jpg',
+	:price => 49.52)
+Product.create(:title => 'Programming Ruby 1.2',
+	:description =>
+	%{<p>
+        Ruby is the fastest growing and most exciting dynamic language out
+        there. If you need to get working programs delivered fast, you should
+        add Ruby to your toolbox.
+	</p>},
+	:image_url => 'ror3.jpg',
+	:price => 49.52)
+
